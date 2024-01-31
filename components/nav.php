4a8f91a6417ab753eed193eb7fa30c3958b30621
Darby <dssumner@nic.edu>
2024-01-31 15:49:20 -0800

    Aligning nav header

--- components/nav.php
+++ components/nav.php
@@ -1,15 +1,18 @@
+<div class="header">
+
+<div>
+    <a href="index.php">
+            <img src="assets/images/DuckingCool.png"  height="200px" alt="Design by Darbs Logo">
+    </a>
+</div>
+
 <div class="nav-menu">
     <nav>
-        <a href="index.php">
-            <img src="assets/images/DuckingCool.png" width="auto" height="300px" alt="Design by Darbs Logo">
-        </a>
-        
-    <div class="nav-links">
+       <div class="nav-links">
         <a href="index.php">Home</a>
         <a href="profile.php">Profile</a>
         <a href="create-duck.php">Create a Duck</a>
     </div>  
-       
-    
     </nav>
 </div>
+</header>
