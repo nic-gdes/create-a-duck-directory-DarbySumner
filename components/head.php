137e64e6ae93ca84253209f6c5e4dc24f6f85443
Darby <dssumner@nic.edu>
2024-01-31 15:16:35 -0800

    idk trying to fix things

--- components/head.php
+++ components/head.php
@@ -2,6 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ducks4Life</title>
-    <link rel="sylesheet" href="assets/css/style.css">
+    <link rel="sylesheet" href="/assets/css/style.css">
     
 </head>
--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-<?php include('./components/head.php');
-      include('./components/nav.php');
-?>
+<?php include('./components/head.php');?>
+<?php  include('./components/nav.php');?>
     <main>
 
     </main> 
