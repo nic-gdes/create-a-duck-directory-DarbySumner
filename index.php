cd143cf8cffdcd7f63d38d5d5d0a19b2c93dabbd
Darby <dssumner@nic.edu>
2024-02-04 14:36:09 -0800

    adding cards for create a duck

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <body>
+
 <?php include('./components/head.php');?>
 <?php  include('./components/nav.php');?>
-    <main>
 
-    </main> 
+<main>
+    <div class="card">
+    <img src="assets/images/Duck1.png" alt="duck1" width="200px">
+    <div class="container">
+        <h4><b>Duck Name</b></h4> 
+        <p>Favorite Foods</p>
+        <p>Item 1</p>
+        <p>Item 2</p>
+        <p>Item 3</p>
+    </div>
+    </div>
+
+    <div class="card">
+    <img src="assets/images/Duck2.png" alt="duck2" width="200px">
+    <div class="container">
+        <h4><b>Duck Name</b></h4> 
+        <p>Favorite Foods</p>
+        <p>Item 1</p>
+        <p>Item 2</p>
+        <p>Item 3</p>
+
+    </div>
+    </div>
+
+    <div class="card">
+    <img src="assets/images/Duck3.png" alt="duck3" width="200px">
+    <div class="container">
+        <h4><b>Duck Name</b></h4> 
+        <p>Favorite Foods</p>
+        <p>Item 1</p>
+        <p>Item 2</p>
+        <p>Item 3</p> 
+    </div>
+    </div>
+</main> 
     <?php include('./components/footer.php'); ?>
 </body>
 </html>
