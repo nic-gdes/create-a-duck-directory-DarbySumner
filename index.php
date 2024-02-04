71936ef0d6cf3902c6ca22a9d760a1bb30a5d66a
Darby <dssumner@nic.edu>
2024-02-04 14:48:58 -0800

    Trying to get styling to work

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
 <body>
 
 <?php include('./components/head.php');?>
-<?php  include('./components/nav.php');?>
+<?php include('./components/nav.php');?>
 
 <main>
+
+    <div class="welcome">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </div>
+
     <div class="card">
     <img src="assets/images/Duck1.png" alt="duck1" width="200px">
     <div class="container">
